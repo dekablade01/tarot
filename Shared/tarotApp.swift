7be2855e643d17a20eeb16d74b0d6a01900a783c
Issarapong Poesua <dekablade01@gmail.com>
2022-02-15 11:56:55 +0700

    Initial commit

---
+++ Shared/tarotApp.swift
@@ -0,0 +1,17 @@
+//
+//  tarotApp.swift
+//  Shared
+//
+//  Created by Issarapong Poesua on 15/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct tarotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
